6a16b340b0131aa1688cde21add1c953fa612c45
Olaf Meyer <olaf.meyer@scotec-software.com>
2026-01-27 17:25:42 +0100

    Migrate installer to WixSharp-wix4 with WPF dialogs
    Updated the installer project to use WixSharp-wix4 with WPF-based dialogs. Changed the target framework to `net4.7.2` and enabled WPF support. Refactored `Installer.cs` to use `ManagedProject` and added a custom UI flow with dialogs for installation. Introduced a new `InstallScopeDialog` for selecting installation scope. Updated the solution structure to include the installer project under a new `/Installation/` folder.

---
+++ Installation/Bim.FamilyManager.Installer/InstallScopeDialog.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WixSharp;
+using WixSharp.UI.WPF;
+
+namespace Bim.FamilyManager.Installer
+{
+    /// <summary>
+    /// Interaction logic for InstallScopeDialog.xaml
+    /// </summary>
+    public partial class InstallScopeDialog : UserControl, IWpfDialogContent
+    {
+        public InstallScopeDialog()
+        {
+            InitializeComponent();
+        }
+
+        public void Init(CustomDialogBase parentDialog)
+        {
+        }
+    }
+}
--- Installation/Bim.FamilyManager.Installer/Installer.cs
+++ Installation/Bim.FamilyManager.Installer/Installer.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WixSharp;
+using ExitDialog = WixSharp.UI.WPF.ExitDialog;
+using InstallDirDialog = WixSharp.UI.WPF.InstallDirDialog;
+using ProgressDialog = WixSharp.UI.WPF.ProgressDialog;
+using WelcomeDialog = WixSharp.UI.WPF.WelcomeDialog;
 
 namespace Bim.FamilyManager.Installer
 {
@@ -11,12 +15,38 @@ namespace Bim.FamilyManager.Installer
     {
         public static void Main(string[] args)
         {
-            var project = new Project("MyProduct",
-                new Dir(@"%ProgramFiles%\My Company\My Product",
-                    new File(@"Files\Docs\Manual.txt"),
-                    new File(@"Files\Bin\MyApp.exe")));
+            var project = new ManagedProject("Bim.FamilyManager",
+                new Dir(@"%ProgramFiles%\Bim.FamilyManager", new Files(@"..\..\..\..\Source\Bim.FamilyManager\bin\x64\Debug\net8.0-windows\*.*")));
 
-            project.GUID = new Guid("6f330b47-2577-43ad-9095-1861ba25889b");
+            project.UpgradeCode = new Guid("6B4E2EEC-E9E3-4AC1-9A1F-83F605B543BE");
+            //project.GUID = new Guid("91E63B96-01C1-481E-9334-50EF5C48DEDF");
+            project.GUID =Guid.NewGuid();
+ 
+            project.Platform = Platform.x64;
+
+            //TODO: Set version dynamically
+            project.Version = new Version("1.0.0");
+
+            var ui = project.ManagedUI = new ManagedUI();
+            ui.InstallDialogs.Add<WelcomeDialog>()
+                .Add<InstallDirDialog>()
+                .Add<CustomDialogWith<InstallScopeDialog>>()
+                .Add<ProgressDialog>()
+                .Add<ExitDialog>();
+
+
+
+            project.UIInitialized += e =>
+            {
+                // Since the default MSI localization data has no entry for 'CustomDlgTitle' (and other custom labels) we
+                // need to add this new content dynamically. Alternatively, you can use WiX localization files (wxl).
+
+                MsiRuntime runtime = e.ManagedUI.Shell.MsiRuntime();
+
+                runtime.UIText["CustomDlgTitle"] = "Select Installation Scope";
+                runtime.UIText["CustomDlgTitleDescription"] = "Please select the installation scope.";
+            };
+
 
             Compiler.BuildMsi(project);
         }
